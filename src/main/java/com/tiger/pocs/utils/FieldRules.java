c630ecf8c55a2c99dcf0cefb8909eb48473fe132
Rikenson JACQUES <9138359+rikenson@users.noreply.github.com>
2024-08-05 08:40:53 -0400

    Update FieldRules.java

--- src/main/java/com/tiger/pocs/utils/FieldRules.java
+++ src/main/java/com/tiger/pocs/utils/FieldRules.java
@@ -1,1 +1,16 @@
+package labs.rikenson.jungle.kernel.utils;
+
+public class FieldRules {
+    public final Class<?> fieldType;
+    public final boolean nullable;
+    public final Integer minLength;
+    public final Integer maxLength;
+
+    public FieldRules(Class<?> fieldType, boolean nullable, Integer minLength, Integer maxLength) {
+        this.fieldType = fieldType;
+        this.nullable = nullable;
+        this.minLength = minLength;
+        this.maxLength = maxLength;
+    }
+}
 
